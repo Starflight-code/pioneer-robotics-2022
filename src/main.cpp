1781ce68e8b24050859376ca0996c980260769d1
Starflight <sparkr123@gmail.com>
2022-10-11 21:24:23 -0500

    Comments and Commented Code Removal
    Added comments, explaining certain logic areas and variables. Removed some code which will never be used.

--- src/main.cpp
+++ src/main.cpp
@@ -27,15 +27,13 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
+
+	// Sets the background to the image contained within waifu_elijah.c
 lv_obj_t* background = lv_img_create(lv_scr_act(), NULL);
 LV_IMG_DECLARE(waifu_elijah);
 lv_img_set_src(background, &waifu_elijah);
 lv_obj_set_size(background, 480, 240);
 lv_obj_align(background, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
-	//pros::lcd::initialize();
-	//pros::lcd::set_text(1, "Hello PROS User!");
-
-	//pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -85,54 +83,59 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	// Imports the Controller and maps it to the variable master
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 
+	// Imports the motors and maps them to variables
+	//pros::Motor motor_name(port);
 	pros::Motor front_left(12);
 	pros::Motor front_right(11);
 	pros::Motor rear_left(2);
 	pros::Motor rear_right(1);
 	pros::Motor flywheel(3);
 	pros::Motor flywheel_2(4);
-	//Control Variables
+
+	//Used within control/logic structures
 	bool held;
 	bool flywheelstate = false;
 
 
 	while (true) {
 		
+	// Test code for randomly setting the screen to different colors. May violate vex rules, do not enable when competing.
+	/*
 	//pros::screen::set_pen(RGB2COLOR((char) (rand() % 255 + 0),(char)  (rand() % 255 + 0), (char) (rand() % 255 + 0)));
 	//pros::screen::fill_rect(0,0,1000,300);
 	//pros::screen::erase();
-		/*pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
 */
+		// Sets the motors to the values of the analog sticks. 
 		front_left = master.get_analog(ANALOG_LEFT_Y);
 		front_right = -1 * master.get_analog(ANALOG_RIGHT_Y);
 		rear_left = master.get_analog(ANALOG_LEFT_Y);
 		rear_right = -1 * master.get_analog(ANALOG_RIGHT_Y);
 
+		// Toggle-able flywheel logic (Hold DIGITAL_A on the controller to activate/deactivate the flywheel)
 		held = master.get_digital(pros::E_CONTROLLER_DIGITAL_A) == 1 && !held;
+		// This is a branching version of the above code. Switch to this only if the branchless version doesn't work. (Branchless code is more efficient)
 		/*if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A) == 1 && !held) { 
 			held = true;
 		}*/
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A) == 0 && held) { 
 		if (flywheelstate == true) {
+		// Flywheel is toggled OFF
 		flywheel = 0;
 		flywheel_2 = 0;	
 		//std::cout << "Flywheel Off";
 		} else {
+		// Flywhhel is toggled ON
 		flywheel = 127;
 		flywheel_2 = -127;
 		//std::cout << "Flywheel On";
 		}
 		}
-
-
-
-		//pros::lcd::set_background_color((char) (rand() % 255 + 0),(char)  (rand() % 255 + 0), (char) (rand() % 255 + 0));
-		//pros::lcd::initialize();
-		//pros::lcd::clear();
 		pros::delay(50);
 	}
 }
