89486668c409f6526feec6ff4865aba95479f0a9
Alex Jeffrey Riniker <alexjriniker@gmail.com>
2022-11-14 16:47:14 -0600

    Added comments to MtrGroup head file

--- include/ajr/MtrGroup.hpp
+++ include/ajr/MtrGroup.hpp
@@ -1,5 +1,4 @@
-// #include "pros/motors.hpp"
-#include "../main.h"
+#include "pros/motors.hpp"
 
 class MtrGroup
 {
@@ -8,11 +7,24 @@ private:
     std::vector<pros::Motor> motors;
 
 public:
+    /**
+     * Default constructor for weired c++ things
+     */
     MtrGroup();
+
+    /**
+     * Create MtrGroup - Motor Group from parameters
+     * @param count the number of motors in group
+     * @param mtr_ports array of motor ports
+     */
     MtrGroup(const int count, const int mtr_ports[]);
 
+    /**
+     * Alternates by setting every other motor in motors
+     * to reverse. Starting with `true`.
+     */
     void alt_reverse();
 
     void set_speed(int speed);
-    float get_actual_velocity();    
+    float get_actual_velocity();
 };
