098fd7eafe035cffa1a10da12585a7f64101cd1a
Alex Jeffrey Riniker <alexjriniker@gmail.com>
2022-11-14 17:14:56 -0600

    Added comments to MtrGroup head file

--- include/ajr/MtrGroup.hpp
+++ include/ajr/MtrGroup.hpp
@@ -25,6 +25,31 @@ public:
      */
     void alt_reverse();
 
-    void set_speed(int speed);
+    // --------------------------------------------------
+    // Alternative methods for those that would appear on
+    // A normal pros::Motor
+    // --------------------------------------------------
+
+    /**
+     * Sets the voltage of each motor
+     *
+     * @param voltage - The new motor voltage from -127 to 127
+     */
+    void set_speed(int voltage);
+
+    /**
+     * Sets each motor in the reversed value
+     * 
+     * @param reversed True reverses every motor, false motor direction is default
+     */
+    void set_reversed(const bool reversed);
+
+    /**
+     * Gets the average actual velocity of the motors
+     *
+     * NOTE: Takes the absolute value of each motors velocity before adding it
+     *
+     * @return The average actual velocity of the motors
+     */
     float get_actual_velocity();
 };
