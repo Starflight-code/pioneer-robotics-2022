0f552f91b21f0f20cafdd7b38079a2f083138790
Ben Kobiske <sparkr123@gmail.com>
2023-02-27 21:50:38 -0600

    Update motors.cpp
    Fully phased out motor_class's methods and it will now be a wrapper for the motors. Commented out code will be removed once testing is completed.

--- src/motors.cpp
+++ src/motors.cpp
@@ -30,7 +30,7 @@ public:
 };*/
 class MotorGroup {
 private:
-    int current_limiter;
+    // int current_limiter;
     double current_speed;
     std::vector<pros::Motor> motors = {};
 
@@ -50,7 +50,7 @@ public:
         // to a reversed one bool initial_reverse_state tells our initializer what
         // the first (or all values for non-alternating) reverse state will be. For
         // alternating, the next reverse state will always be opposite of this one.
-        current_limiter = 100;
+        // current_limiter = 100;
         if(alternating) {
             bool t =
                 initial_reverse_state;                            // Reverse state is used, as the contructor
@@ -79,9 +79,9 @@ public:
     /*void debug() {
       motors[0] = 90;
     }*/
-    int getSpeed() { return current_speed; }                                    // Fetches speed from tracker
-    int getLimiter() { return current_limiter; }                                // Fetches limiter from variable
-    void setLimiter(int limiter_setting) { current_limiter = limiter_setting; } // Sets the limiter variable
+    int getSpeed() { return current_speed; } // Fetches speed from tracker
+    // int getLimiter() { return current_limiter; }                                // Fetches limiter from variable
+    // void setLimiter(int limiter_setting) { current_limiter = limiter_setting; } // Sets the limiter variable
     double getPosition() {
         double total = 0;
         for(int i = 0; i < motors.size(); i++) { // Loops for each motor, pulls velocities from each
@@ -105,7 +105,7 @@ public:
     }
 };
 class Motor_Class {
-public: // Phase out old Motor_Class functions (setSpeed, getSpeed, etc), and merely use as a holder for motor group objects
+public: // Phase out old Motor_Class functions (setSpeed, getSpeed, etc), and merely use as a holder for motor group objects - DONE
     MotorGroup leftMotors;
     MotorGroup rightMotors;
     MotorGroup flywheelMotors;
@@ -122,132 +122,133 @@ public:
         rightMotors.init(Robot.rightPorts, Robot.rightAlt_Rev_States[0], Robot.rightAlt_Rev_States[1]);
         flywheelMotors.init(Robot.flywheelPorts, Robot.flywheelAlt_Rev_States[0], Robot.flywheelAlt_Rev_States[1]);
         spinnerMotors.init(Robot.spinnerPorts, Robot.spinnerAlt_Rev_States[0], Robot.spinnerAlt_Rev_States[1]);
-        leftMotors.setLimiter(Robot.limiter);
-        rightMotors.setLimiter(Robot.limiter);
-        flywheelMotors.setLimiter(Robot.limiter);
-        spinnerMotors.setLimiter(Robot.limiter);
-        // launcher.init(Robot.launcher_port);
+        // leftMotors.setLimiter(Robot.limiter); Limiter no longer handled by motor array class
+        // rightMotors.setLimiter(Robot.limiter);
+        // flywheelMotors.setLimiter(Robot.limiter);
+        // spinnerMotors.setLimiter(Robot.limiter);
+        //  launcher.init(Robot.launcher_port);
     }
 
-private:
-    float modifier = 1; // This reduced the motor power by POWER * modifier,
-                        // should be between 0 and 1 or it will return an error.
-    int left_motors = 0;
-    int right_motors = 0;
-    int flywheels = 0;
-    bool init = true;
+    // private:
+    //  float modifier = 1; // This reduced the motor power by POWER * modifier,
+    //   should be between 0 and 1 or it will return an error.
+    //  int left_motors = 0;
+    //  int right_motors = 0;
+    //  int flywheels = 0;
+    //  bool init = true;
 
-private:
-    void setSpeed(int motorSet, int speed) {
-        switch(motorSet) {
-        case 1: // motorSpeed(1, speed) to set left motor group's speed
-            leftMotors.set(speed);
-            break;
-        case 2: // motorSpeed(2, speed) to set the right motor group's speed
-            rightMotors.set(speed);
-            break;
-        case 3: // motorSpeed(3, speed) to set flywheel motor group's speed
-            flywheelMotors.set(speed);
-            break;
-        case 4: // motorSpeed(3, speed) to set spinner motor group's speed
-            spinnerMotors.set(speed);
-            break;
-        default:
+    /* REMOVE AFTER TESTING, this is a major change
+    private:
+        void setSpeed(int motorSet, int speed) {
+            switch(motorSet) {
+            case 1: // motorSpeed(1, speed) to set left motor group's speed
+                leftMotors.set(speed);
+                break;
+            case 2: // motorSpeed(2, speed) to set the right motor group's speed
+                rightMotors.set(speed);
+                break;
+            case 3: // motorSpeed(3, speed) to set flywheel motor group's speed
+                flywheelMotors.set(speed);
+                break;
+            case 4: // motorSpeed(3, speed) to set spinner motor group's speed
+                spinnerMotors.set(speed);
+                break;
+            default:
+                return;
+            }
             return;
-        }
-        return;
-    };
+        };
 
-    int getSpeed(int motorSet) {
-        switch(motorSet) {
-        case 1:
-            return leftMotors.getSpeed(); // Returns the left motor group tracker
-                                          // value (speed of the motor set)
-        case 2:
-            return rightMotors.getSpeed(); // Returns the right motor group tracker
-                                           // value (speed of the motor set)
-        case 3:
-            return flywheelMotors.getSpeed(); // Returns the flywheel motor group tracker value (speed
-                                              // of the motor set)
-        case 4:
-            return spinnerMotors.getSpeed(); // Returns the spinner motor group tracker value (speed
-                                             // of the motor set)
-        default:
-            return 0; // Returns ZERO if an erroneous input was given.
+        int getSpeed(int motorSet) {
+            switch(motorSet) {
+            case 1:
+                return leftMotors.getSpeed(); // Returns the left motor group tracker
+                                              // value (speed of the motor set)
+            case 2:
+                return rightMotors.getSpeed(); // Returns the right motor group tracker
+                                               // value (speed of the motor set)
+            case 3:
+                return flywheelMotors.getSpeed(); // Returns the flywheel motor group tracker value (speed
+                                                  // of the motor set)
+            case 4:
+                return spinnerMotors.getSpeed(); // Returns the spinner motor group tracker value (speed
+                                                 // of the motor set)
+            default:
+                return 0; // Returns ZERO if an erroneous input was given.
+            }
         }
-    }
 
-    float getVelocity(int motorSet) {
-        switch(motorSet) {
-        case 1:
-            return leftMotors.getVelocity();
-            // Returns the left motor group encoder value (average speed
-            // of the motor set)
-        case 2:
-            return rightMotors.getVelocity();
-            // Returns the right motor group encoder value (average speed
-            // of the motor set)
-        case 3:
-            return flywheelMotors.getVelocity(); // Returns the flywheel motor group encoder value
-                                                 // (average speed of the motor set)
-        case 4:
-            return spinnerMotors.getVelocity(); // Returns the spinner motor group encoder value
-                                                // (average speed of the motor set)
-        default:
-            return 0;
+        float getVelocity(int motorSet) {
+            switch(motorSet) {
+            case 1:
+                return leftMotors.getVelocity();
+                // Returns the left motor group encoder value (average speed
+                // of the motor set)
+            case 2:
+                return rightMotors.getVelocity();
+                // Returns the right motor group encoder value (average speed
+                // of the motor set)
+            case 3:
+                return flywheelMotors.getVelocity(); // Returns the flywheel motor group encoder value
+                                                     // (average speed of the motor set)
+            case 4:
+                return spinnerMotors.getVelocity(); // Returns the spinner motor group encoder value
+                                                    // (average speed of the motor set)
+            default:
+                return 0;
+            }
         }
-    }
-    float getPosition(int motorSet) {
-        double total;
-        switch(motorSet) {
-        case 1:
-            return leftMotors.getPosition();
-            // Returns the left motor group encoder value (average speed
-            // of the motor set)
-        case 2:
-            return rightMotors.getPosition();
-            // Returns the right motor group encoder value (average speed
-            // of the motor set)
-        case 3:
-            return flywheelMotors.getPosition();
-            // Returns the flywheel motor group encoder value (average speed
-            // of the motor set)
-        case 4:
-            return spinnerMotors.getPosition();
-            // Returns the flywheel motor group encoder value
-            // (average speed of the motor set)
-        default:
-            return 0;
+        float getPosition(int motorSet) {
+            double total;
+            switch(motorSet) {
+            case 1:
+                return leftMotors.getPosition();
+                // Returns the left motor group encoder value (average speed
+                // of the motor set)
+            case 2:
+                return rightMotors.getPosition();
+                // Returns the right motor group encoder value (average speed
+                // of the motor set)
+            case 3:
+                return flywheelMotors.getPosition();
+                // Returns the flywheel motor group encoder value (average speed
+                // of the motor set)
+            case 4:
+                return spinnerMotors.getPosition();
+                // Returns the flywheel motor group encoder value
+                // (average speed of the motor set)
+            default:
+                return 0;
+            }
         }
-    }
-    float getFastVelocity(int motorSet) {
-        switch(motorSet) {
-        case 1:
-            return leftMotors.getFastVelocity(); // Returns the left motor group encoder value
-                                                 // (speed of first motor in the motor set)
-        case 2:
-            return rightMotors.getFastVelocity(); // Returns the right motor group encoder value
-                                                  // (speed of first motor in the motor set)
-        case 3:
-            return flywheelMotors.getFastVelocity(); // Returns the flywhhel motor group encoder value
+        float getFastVelocity(int motorSet) {
+            switch(motorSet) {
+            case 1:
+                return leftMotors.getFastVelocity(); // Returns the left motor group encoder value
                                                      // (speed of first motor in the motor set)
-        default:
-            return 0;
+            case 2:
+                return rightMotors.getFastVelocity(); // Returns the right motor group encoder value
+                                                      // (speed of first motor in the motor set)
+            case 3:
+                return flywheelMotors.getFastVelocity(); // Returns the flywhhel motor group encoder value
+                                                         // (speed of first motor in the motor set)
+            default:
+                return 0;
+            }
         }
-    }
-    float getFastPosititon(int motorSet) {
-        switch(motorSet) {
-        case 1:
-            return leftMotors.getFastPosition(); // Returns the left motor group encoder value
-                                                 // (speed of first motor in the motor set)
-        case 2:
-            return rightMotors.getFastPosition(); // Returns the right motor group encoder value
-                                                  // (speed of first motor in the motor set)
-        case 3:
-            return flywheelMotors.getFastPosition(); // Returns the flywhhel motor group encoder value
+        float getFastPosititon(int motorSet) {
+            switch(motorSet) {
+            case 1:
+                return leftMotors.getFastPosition(); // Returns the left motor group encoder value
                                                      // (speed of first motor in the motor set)
-        default:
-            return 0;
-        }
-    }
+            case 2:
+                return rightMotors.getFastPosition(); // Returns the right motor group encoder value
+                                                      // (speed of first motor in the motor set)
+            case 3:
+                return flywheelMotors.getFastPosition(); // Returns the flywhhel motor group encoder value
+                                                         // (speed of first motor in the motor set)
+            default:
+                return 0;
+            }
+        }*/
 };
